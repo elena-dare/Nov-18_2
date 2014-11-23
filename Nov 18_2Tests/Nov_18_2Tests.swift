f65e55ba0b7da44f1ba8944cf4b56cc46f171ae9
Elena Da Re <elenadare@Elenas-MacBook-Pro.local>
2014-11-23 16:56:53 -0500

    Initial Commit

---
+++ Nov 18_2Tests/Nov_18_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Nov_18_2Tests.swift
+//  Nov 18_2Tests
+//
+//  Created by Elena Da Re on 11/23/14.
+//  Copyright (c) 2014 Elena Da Re. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Nov_18_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
